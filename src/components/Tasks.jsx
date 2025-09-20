82d84f62b2c66588f1a21f3ae5228f8e9c7f8b8e
Matheus Romão <matheus_henrique_9@hotmail.com>
2025-09-20 11:13:20 -0300

    Adicionando a funcionalidade de task concluida

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -6,7 +6,7 @@ function Tasks(props) {
     <ul className="space-y-4 bg-slate-200 p-6 rounded-md shadow">
       {props.tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
-          <button className="bg-slate-400 w-full text-white p-2 rounded-md">
+          <button className="bg-slate-400 text-left w-full text-white p-2 rounded-md">
             {task.title}
           </button>
           <button className="bg-slate-400 p-2 rounded-md text-white">
