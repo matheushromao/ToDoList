43c74e5613aaa0970af352af6d5082e02dcfb9fe
Matheus Romão <matheus_henrique_9@hotmail.com>
2025-09-20 11:26:31 -0300

    estilizando o click
    Quando clicar irá passar um traço na tarefa concluída, e também adicionando um novo button de delete

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
 
 function Tasks(props) {
   console.log(props);
@@ -6,11 +6,19 @@ function Tasks(props) {
     <ul className="space-y-4 bg-slate-200 p-6 rounded-md shadow">
       {props.tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
-          <button className="bg-slate-400 text-left w-full text-white p-2 rounded-md">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+              task.isCompleted && "line-through"
+            }`}
+          >
             {task.title}
           </button>
           <button className="bg-slate-400 p-2 rounded-md text-white">
             <ChevronRightIcon />
+          </button>
+          <button className="bg-slate-400 p-2 rounded-md text-white">
+            <TrashIcon />
           </button>
         </li>
       ))}
